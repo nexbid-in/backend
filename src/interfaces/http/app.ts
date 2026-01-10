ce929d828c0066619c5cd4a535e96301508b17a5
Vaisakh R <vaisakh4693@gmail.com>
2026-01-10 13:44:53 +0530

    feat: add request logging with morgan

--- src/interfaces/http/app.ts
+++ src/interfaces/http/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 
 export const createApp = () => {
     const app = express();
 
+    app.use(morgan("dev"));
     app.use(express.json());
 
     app.get("/health", (req, res) => {
