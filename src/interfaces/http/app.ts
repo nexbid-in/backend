290c39d3d67841474bf0c954c03a3890c4c332a1
Vaisakh R <vaisakh4693@gmail.com>
2026-01-15 21:18:00 +0530

    feat: integrate database with api endpoints for testing

--- src/interfaces/http/app.ts
+++ src/interfaces/http/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import morgan from "morgan";
 
+import { prisma } from "../../infrastructure/database/prisma";
+import { logger } from "../../infrastructure/logging/logger";
+
 
 export const createApp = () => {
     const app = express();
@@ -14,6 +17,30 @@ export const createApp = () => {
             timestamp: new Date().toISOString(),
             uptime: process.uptime()
         });
+    });
+
+    app.post("/register", async (req, res) => {
+        try {
+            const { name, email, password } = req.body;
+
+            if (!email || !password) {
+                return res.status(400).json({ error: "Email and password are required!"});
+            }
+
+            const user = await prisma.user.create({
+                data: { email, name, password }
+            });
+
+            res.status(201).json(user);
+        } catch (error: any) {
+            logger.error(error)
+            res.status(500).json({message: error.message});
+        }
+    });
+
+    app.get("/getusers", async (req, res) => {
+        const users = await prisma.user.findMany();
+        res.status(200).json(users);
     });
 
     return app;
